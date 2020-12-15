6b2e5034b6cb886a0b2e1a003bd2cfb323836905
Heshanibandaranayake <heshanibandaranayake@gmail.com>
2020-12-15 10:07:46 +0530

    change request method as POST

--- src/main/java/com/api/rest/controller/WebController.java
+++ src/main/java/com/api/rest/controller/WebController.java
@@ -1,13 +1,14 @@
 package com.api.rest.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.api.rest.model.SampleResponce;
 
 @RestController
 public class WebController {
-    @RequestMapping("/sample")
+    @RequestMapping(value = "/sample", method = RequestMethod.POST)
     public SampleResponce Sample(@RequestParam(value = "name",
             defaultValue = "Robot") String name) {
         SampleResponce response = new SampleResponce();
